3de12acf9f5ed5b2865d066f04b17c90fc800aa1
fatalevrey <fatalevrey@gmail.com>
2022-07-06 18:04:18 +0300

    add function encrypt

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const encrypt = (message = "") => {
-  return message;
+const keys = {'б': 'щ', 'в': 'ш', 'г': 'ч', 'д': 'ц', 'ж': 'х', 'з': 'ф', 'к': 'т', 'л': 'с', 'м': 'р', 'н': 'п'}
+const crypt = (message = "") => {
+  symblist = message.split("");
+  for (var i = 0; i < symblist.length; i++) {
+   for (let value of Object.keys(keys)){
+    if (symblist[i] == value ) {
+      symblist[i] = keys[value]
+    } else if (symblist[i] == keys[value] ) {
+      symblist[i] = value 
+    }
+  }
+}
+  return symblist.join("");
 };
 
-const decrypt = (message = "") => {
-  return message;
-}
-
-module.exports = { encrypt, decrypt }
+module.exports = { crypt }
